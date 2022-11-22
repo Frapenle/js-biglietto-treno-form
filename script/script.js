f01d449c9093bcf5f5df664d587a990e16b19084
francescotrudu <francescotrudu84@gmail.com>
2022-11-22 15:46:40 +0100

    Upload index.html and script.js

---
+++ script/script.js
@@ -0,0 +1,17 @@
+// variabile km da percorrere
+let km = document.querySelector("input#km");
+console.log(parseInt(km.value));
+
+// variabile età
+let age = document.querySelector("input#user-age");
+console.log(parseInt(age.value));
+
+// price x km
+let priceKm = 0.2762
+
+// bottone per il calcolo ticket
+const ticket = document.getElementById("ticketbtn");
+
+// se minorenne sconto 17.5%
+
+// se over 65 sconto 33.3%
